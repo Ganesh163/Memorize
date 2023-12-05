622f2266e12f84cd873daf51ecc858dac77483ed
Sai Ganesh <saiganesh.020701@gmail.com>
2023-12-06 00:48:32 +0530

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by sgv on 01/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView(isFaceUp: true)
+            CardView()
+        }
+        .padding()
+        .foregroundColor(.orange)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View {
+        if isFaceUp {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 10)
+                    .strokeBorder(lineWidth: 2)
+                Text("🤑").font(.largeTitle)
+            }
+        }else{
+            RoundedRectangle(cornerRadius: 10)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
